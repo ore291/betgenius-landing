62748acb9956f610be36f46083f0e5a56ea94e1e
ore291 <oreoluwapadonu@gmail.com>
2024-09-30 20:45:22 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/header";
+import Header from "./components/Header";
 import Hero from "./components/Hero";
 import "./App.css";
 import Features from "./components/Features";
